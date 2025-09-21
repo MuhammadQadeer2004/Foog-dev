dde514389920a3245b1ad830954b3c932d8acad2
Muhammad Qadeer <qadeer142004@gmail.com>
2025-09-22 02:03:50 +0500

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Chooes from a diverse manu featuring a delectable array of dishes crafted with the finest ingredients and culinary experties.Our mission is to satisfy your craving and elevate your dining experiences,one delicious meal at the time</p>
+        <button>view Menu</button>
+      </div>
       
     </div>
   )
