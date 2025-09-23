92e38046c1e5c9d30477f39ffe72bede05ce6a7d
Muhammad Qadeer <qadeer142004@gmail.com>
2025-09-24 01:27:40 +0500

    first commit

--- src/context/storeContext.jsx
+++ src/context/storeContext.jsx
@@ -20,9 +20,20 @@ const StoreContextProvider = (props) => {
         setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    useEffect(() => {
-        console.log(cartItems);
-    }), [cartItems]
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+    // useEffect(() => {
+    //     console.log(cartItems);
+    // }), [cartItems]
 
 
     const contextValue = {
@@ -30,7 +41,8 @@ const StoreContextProvider = (props) => {
         cartItems,
         setCartItem,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
 
     return (
